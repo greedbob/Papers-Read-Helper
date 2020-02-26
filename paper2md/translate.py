067b4e11bc9fb6b6ff0eebd6f7075ccff18fbf08
greedbob <greedbob@foxmail.com>
2020-02-27 03:47:36 +0800

    :bug: fix translation error for citation

--- paper2md/translate.py
+++ paper2md/translate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from googletrans import Translator
+import re
 
 
 class Translate:
@@ -11,7 +12,11 @@ class Translate:
     def translate(self, content):
         translator = Translator(service_urls=['translate.google.cn'])
         for line in content:
-            self.trans_content.append(line.strip('\n') + '  \n')
+            if not line:
+                self.trans_content.append(line.strip('\n') + '  \n')
+            else:
+                self.trans_content.append(line)
             if len(line) > 6 and line[0] not in self.not_trans_dic:
+                line = re.sub(r'((\d{1,2})(,|(\.)))|(((\d{1,2})[-–](\d{1,2}))(,|(\.)))', '.', line)
                 translation = translator.translate(line.strip(), dest='zh-cn').text
                 self.trans_content.append(translation + '\n\n')
